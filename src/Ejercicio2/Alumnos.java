b492742c2d830c12f0a9d598c89d528c622f2983
rubem197 <ruben.lindes@gmail.com>
2022-02-21 21:12:33 +0100

    awa

--- src/Ejercicio2/Alumnos.java
+++ src/Ejercicio2/Alumnos.java
@@ -5,47 +5,68 @@ public class Alumnos {
     private String nombres;
     private int notas;
 
-    public Alumnos(String nombres){
-        this.nombres=nombres;
-        this.notas=(int)Math.round(Math.random()*9+1);
+    public Alumnos(String nombres) {
+        this.nombres = nombres;
+        this.notas = (int) Math.round(Math.random() * 9 + 1);
     }
 
-    public static int notaMedia(Alumnos[] A){
+    public static int notaMedia(Alumnos[] A) {
         int calificaciones = 0;
-        int contador=0;
-        for (int i =0; i< A.length;i++){
-            calificaciones=calificaciones+A[i].getNotas();
+        int contador = 0;
+        for (int i = 0; i < A.length; i++) {
+            calificaciones = calificaciones + A[i].getNotas();
             contador++;
         }
-        int notamedia=calificaciones/contador;
+        int notamedia = calificaciones / contador;
+        System.out.println(notamedia);
         return notamedia;
     }
-    public static int notaAlta(Alumnos []A){
-        int maximaNota=0;
-        for (int i =0; i< A.length;i++){
-            if (A[i].getNotas()>maximaNota){
-                maximaNota=A[i].getNotas();
+
+    public static int notaAlta(Alumnos[] A) {
+        int maximaNota = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i].getNotas() > maximaNota) {
+                maximaNota = A[i].getNotas();
             }
         }
+        System.out.println(maximaNota);
         return maximaNota;
     }
-    public static int notaBaja(Alumnos []A){
-        int notaMinima=10;
-        for (int i =0; i< A.length;i++){
-            if (A[i].getNotas()<notaMinima){
-                notaMinima=A[i].getNotas();
+
+    public static int notaBaja(Alumnos[] A) {
+        int notaMinima = 10;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i].getNotas() < notaMinima) {
+                notaMinima = A[i].getNotas();
             }
         }
+        System.out.println(notaMinima);
         return notaMinima;
     }
-    public static void encimaMedia(Alumnos []A) {
+
+    public static void encimaMedia(Alumnos[] A) {
+        int contador = 0;
         for (int i = 0; i < A.length; i++) {
+
             if (A[i].getNotas() > notaMedia(A)) {
-                System.out.println("El alumno "+A[i].getNombres() +" con nota " +A[i].getNotas());
+                contador++;
+
+                System.out.println("El alumno " + A[i].getNombres() + " con nota " + A[i].getNotas());
             }
         }
+        System.out.println(contador + "Alumnos han sacado por encima de la media");
     }
 
+    public static void debajoMedia(Alumnos[] A) {
+        int contador = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i].getNotas() < notaMedia(A)) {
+                contador++;
+                System.out.println("El alumno " + A[i].getNombres() + " con nota " + A[i].getNotas());
+            }
+        }
+        System.out.println(contador + " alumnos han sacado por debajo de la media");
+    }
 
 
     public int getNotas() {
--- src/Ejercicio2/Ejercicio2.java
+++ src/Ejercicio2/Ejercicio2.java
@@ -1,18 +1,35 @@
 package Ejercicio2;
 
+import java.util.Scanner;
+
 import static Ejercicio2.Alumnos.*;
 import static Ejercicio2.Alumnos.notaMedia;
 
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-        Alumnos a1= new Alumnos("David");
-        Alumnos a2= new Alumnos("Angel");
-        Alumnos a3= new Alumnos("Alex");
-        Alumnos a4= new Alumnos("Ruben");
+        Scanner sc = new Scanner(System.in);
+        Alumnos a1 = new Alumnos("David");
+        Alumnos a2 = new Alumnos("Angel");
+        Alumnos a3 = new Alumnos("Alex");
+        Alumnos a4 = new Alumnos("Ruben");
 
-        Alumnos []alumnos = new Alumnos[]{a1,a2,a3,a4};
+        Alumnos[] alumnos = new Alumnos[]{a1, a2, a3, a4};
 
-        encimaMedia(alumnos);
+
+
+        int opc;
+        do {
+            System.out.println("introduce una opcion del menu, para salir pulsa 0");
+            opc = sc.nextInt();
+            switch (opc) {
+                case 1 -> notaMedia(alumnos);
+                case 2 -> notaAlta(alumnos);
+                case 3 -> notaBaja(alumnos);
+                case 4 -> encimaMedia(alumnos);
+                case 5 -> debajoMedia(alumnos);
+                default -> System.out.println("Has pulsado un numero invalido introduce otro");
+            }
+        }while(opc !=0);
     }
 }
