58b5f04bece3ea848317a1268f9713c1d3de77ae
ruben Lindes Roldan <ruben.lindes@iesnervion.es>
2022-02-22 13:19:25 +0100

    awa

---
+++ src/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,14 @@
+package Ejercicio4;
+
+import static Ejercicio4.Erasto.generarArray;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+
+
+
+        for (int x : generarArray()){
+            System.out.println(x);
+        }
+    }
+}
