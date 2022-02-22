bfb89d0b6e95d1e27a9ca3d44de485510aac0d12
rubem197 <ruben.lindes@gmail.com>
2022-02-22 21:15:38 +0100

    awa

--- src/Ejercicio4/Ejercicio4.java
+++ src/Ejercicio4/Ejercicio4.java
@@ -1,12 +1,11 @@
 package Ejercicio4;
 
+import java.util.Scanner;
+
 import static Ejercicio4.Erasto.generarArray;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-
-
-
         generarArray();
     }
 }
--- src/Ejercicio4/Erasto.java
+++ src/Ejercicio4/Erasto.java
@@ -10,6 +10,7 @@ public class Erasto {
         int numero = sc.nextInt();
         int[] primos = new int[generarTamano(numero)];
         generarValores(primos, numero);
+        generarCriba(primos, numero);
         return primos;
     }
 
@@ -25,7 +26,7 @@ public class Erasto {
 
     public static void generarValores(int[] A, int numero) {
         int tamano = 0;
-        for (int i = 0; i < numero; i++) {
+        for (int i = 3; i < numero; i++) {
             if (i % 2 != 0) {
                 A[tamano] = i;
                 tamano++;
@@ -33,5 +34,29 @@ public class Erasto {
         }
     }
 
+    public static void generarCriba(int[] A, int numero) {
+        int j = 0;
+        int contador = 0;
+        while (j < A.length) {
+            for (int i = 2; i < numero; i++) {
+                if (3 * i == A[j] || 5 * i == A[j] || 7 * i == A[j]) {
+                    contador++;
+                    A[i]=0;
+                }
+            }
+            j++;
+        }
+        int[] B = new int[A.length - contador];
+        int posicion=0;
+        for (int x=0;x<A.length;x++){
+            if (A[x]!=0){
+                B[posicion]=A[x];
+                posicion++;
+            }
+        }
+        for (int x:B){
+            System.out.println(x);
+        }
+    }
 
 }
